7dd23618adebedfafd75649433783681398b4318
Nelson Chandimba da Silva <dismao16@gmail.com>
2024-04-15 14:13:16 +0100

    Uso de retentativas de chamadas num serviço indisponível ou que não responde no tempo devido

--- authuser/src/main/java/com/ead/authuser/client/CourseClient.java
+++ authuser/src/main/java/com/ead/authuser/client/CourseClient.java
@@ -3,11 +3,13 @@ package com.ead.authuser.client;
 import com.ead.authuser.dto.CourseDTO;
 import com.ead.authuser.dto.ResponsePageDTO;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Log4j2
@@ -28,7 +32,7 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URL_COURSE;
 
-
+    @Retry(name = "retryInstance", fallbackMethod = "retryFallback")
     public Page<CourseDTO> getAllCoursesByUser(final UUID userId, Pageable pageable) {
 
         ResponseEntity<ResponsePageDTO<CourseDTO>> result = null;
@@ -37,6 +41,7 @@ public class CourseClient {
 
         log.debug("Request URL: {}", url);
         log.info("Request URL: {}", url);
+        System.out.println("--- Start Request ao Course Microservice ---");
 
         try {
             ParameterizedTypeReference<ResponsePageDTO<CourseDTO>> responseType = new ParameterizedTypeReference<>() {};
@@ -51,6 +56,12 @@ public class CourseClient {
         log.info("Ending request /courses userId {} ", userId);
 
         return result.getBody();
+    }
+
+    public Page<CourseDTO> retryFallback(final UUID userId, Pageable pageable, Throwable exception) {
+        log.error("Inside retry retryFallback, cause - {}", exception.toString());
+        List<CourseDTO> courseDTOS = new ArrayList<>();
+        return new PageImpl<>(courseDTOS);
     }
 
 }
